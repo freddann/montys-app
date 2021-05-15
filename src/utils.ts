76054dd6ec935e34d4fb5ab8ded24d0fdca9bcc8
Fredrik Eklund <fredrik.eklund@telia.no>
2021-05-15 14:05:14 +0200

    remove unused function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { Event } from "./types";
-
 export function getRandomElement<T>(list: T[]): T {
   return list[Math.floor(Math.random() * list.length)];
 }
@@ -10,13 +8,4 @@ export function range(total: number): number[] {
     result.push(i);
   }
   return result;
-}
-export function generateSequence(winningIndex: number, boxRange: number[]): { sequence: Event[], userFinalPick: number } {
-  const userPick = getRandomElement(boxRange);
-  const montyPick = getRandomElement(boxRange.filter(v => v !== userPick && v !== winningIndex));
-  const userFinalPick = getRandomElement(boxRange.filter(v => v !== userPick && v !== montyPick));
-  return {
-    sequence: [{ name: "userPick", index: userPick }, { name: "montyPick", index: montyPick }, { name: "userFinal", index: userFinalPick }],
-    userFinalPick,
-  };
 }
