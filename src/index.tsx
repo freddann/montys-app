7e9059f3fff662bfeec20e24d9350dd0511f731f
Fredrik Eklund <fredrik.eklund@telia.no>
2021-05-15 11:45:52 +0200

    Refactor: use box ranges consistently

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { NUM_BOXES } from "./config";
-import { generateSequence, getRandomIndex } from "./utils";
+import { generateSequence, range, getRandomElement } from "./utils";
 
 function App() {
   const [counter, setCounter] = React.useState(0);
-  const winningIndex = React.useMemo(() => getRandomIndex(NUM_BOXES), [counter]);
-  const sequence = React.useMemo(() => generateSequence(winningIndex), [counter]);
+  const [delay, setDelay] = React.useState(200);
+  const boxRange = React.useMemo(() => range(NUM_BOXES), [range, NUM_BOXES]);
+  const winningIndex = React.useMemo(() => getRandomElement(boxRange), [counter, boxRange]);
+  const sequence = React.useMemo(() => generateSequence(winningIndex, boxRange), [counter, boxRange]);
   return (
     <div>
       <div>Winning index: {winningIndex}</div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,19 @@
-import { NUM_BOXES } from "./config";
 import { Event } from "./types";
 
-export function getRandomIndex(total: number): number {
-  return Math.floor(Math.random() * total);
+export function getRandomElement<T>(list: T[]): T {
+  return list[Math.floor(Math.random() * list.length)];
 }
 
-function getRandomElement<T>(list: T[]): T {
-  return list[getRandomIndex(list.length)];
-}
-
-function range(total: number): number[] {
+export function range(total: number): number[] {
   const result  = [];
   for (let i = 0; i < total; i++) {
     result.push(i);
   }
   return result;
 }
-export function generateSequence(winningIndex: number): Event[] {
-  const options = range(NUM_BOXES);
-  const userPick = getRandomElement(options);
-  const montyPick = getRandomElement(options.filter(v => v !== userPick && v !== winningIndex));
-  const userFinal = getRandomElement(options.filter(v => v !== userPick && v !== montyPick));
-  return [{ name: "userPick", index: userPick }, {name: "montyPick", index: montyPick }, { name: "userFinal", index: userFinal }];
+export function generateSequence(winningIndex: number, boxRange: number[]): Event[] {
+  const userPick = getRandomElement(boxRange);
+  const montyPick = getRandomElement(boxRange.filter(v => v !== userPick && v !== winningIndex));
+  const userFinal = getRandomElement(boxRange.filter(v => v !== userPick && v !== montyPick));
+  return [{ name: "userPick", index: userPick }, { name: "montyPick", index: montyPick }, { name: "userFinal", index: userFinal }];
 }
